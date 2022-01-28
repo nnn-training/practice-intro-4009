d853c9770e124858646c5104d8d6020a4eb4f329
compmac-sblo <>
2022-01-28 20:25:10 +0900

    WebSocket の接続時と切断時に文字列を表示

--- app/entry.js
+++ app/entry.js
@@ -22,3 +22,6 @@ const socket = io('http://localhost:8000');
 socket.on('server-status', (data) => {
   loadavg.text(data.loadavg.toString());
 });
+
+socket.on('connect', () => {console.info("接続しまんた");});
+socket.on('disconnect', () => {console.info("切断しまみた");});
